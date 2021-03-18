ea95ec3eb334777d2f834152bfaa28f57da45bdb
Rodolpho Alcantara <rodolphoalcantara@outlook.com>
2021-03-18 01:03:55 -0300

    Ajustandos os Filter para poder realizar o deploy em outro servlet container

--- Java/servlet-gerenciador/src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
+++ Java/servlet-gerenciador/src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -18,9 +18,12 @@ import javax.servlet.http.HttpSession;
 //@WebFilter("/entrada")
 public class AutorizacaoFilter implements Filter {
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+
+	@Override
+	public void destroy() {}
+	
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		
 		System.out.println("AutorizacaoFilter");
--- Java/servlet-gerenciador/src/br/com/alura/gerenciador/servlet/ControladorFilter.java
+++ Java/servlet-gerenciador/src/br/com/alura/gerenciador/servlet/ControladorFilter.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
@@ -20,10 +19,14 @@ import br.com.alura.gerenciador.acao.Acao;
  */
 //@WebFilter("/entrada")
 public class ControladorFilter implements Filter {
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
+	@Override
+	public void destroy() {}
+
+	
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		System.out.println("ControladorFilter");
 		
