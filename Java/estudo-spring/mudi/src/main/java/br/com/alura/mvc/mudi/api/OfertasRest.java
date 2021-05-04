ae9d1fff5701e493a15681d1a4f8c5671baf211e
Rodolpho Alcântara <rodolphoalcantara@outlook.com>
2021-05-03 21:05:36 -0300

    Utilização de Vue.js para criação da pagina de pedidos e consumindo a api do projeto mudi

--- Java/estudo-spring/mudi/src/main/java/br/com/alura/mvc/mudi/api/OfertasRest.java
+++ Java/estudo-spring/mudi/src/main/java/br/com/alura/mvc/mudi/api/OfertasRest.java
@@ -2,7 +2,11 @@ package br.com.alura.mvc.mudi.api;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +22,8 @@ public class OfertasRest {
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
-	
-	public Oferta criaOferta(RequisicaoNovaOferta requisicao) {
+	@PostMapping
+	public Oferta criaOferta(@Valid @RequestBody RequisicaoNovaOferta requisicao) {
 		Optional<Pedido> pedidoBuscado = pedidoRepository.findById(requisicao.getPedidoId());
 		
 		if(!pedidoBuscado.isPresent()) {
