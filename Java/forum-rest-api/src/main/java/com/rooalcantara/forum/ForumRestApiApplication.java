33b4445e779c5620cad62796983489128afc0253
Rodolpho Alcântara <rodolphoalcantara@outlook.com>
2021-05-13 19:50:11 -0300

    criação do projeto para estudos de rest apis

---
+++ Java/forum-rest-api/src/main/java/com/rooalcantara/forum/ForumRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.rooalcantara.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumRestApiApplication.class, args);
+	}
+
+}
